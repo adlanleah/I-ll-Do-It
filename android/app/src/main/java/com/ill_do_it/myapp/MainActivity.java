7a97b1449eb95b794f6912a38c2567354d6f1265
codrone <ronniesolomonmpala@gmail.com>
2026-02-24 13:19:53 +0300

    added android and test run woorks

---
+++ android/app/src/main/java/com/ill_do_it/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ill_do_it.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ill-do-it.myapp',
+  appId: 'com.ill_do_it.myapp',
   appName: 'Ill Do It',
   webDir: 'www'
 };
